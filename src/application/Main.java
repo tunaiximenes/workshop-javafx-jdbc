f1168399fde071b3b90b53567e358ca980ce8a15
Tunai Ximenes <tunai.ximenes@gmail.com>
2021-08-28 22:19:48 -0300

    About View

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,6 +9,9 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Main extends Application {
+
+    private static Scene mainScene;
+
     @Override
     public void start(Stage primaryStage) {
         try {
@@ -18,7 +21,7 @@ public class Main extends Application {
             scrollPane.setFitToHeight(true);
             scrollPane.setFitToWidth(true);
 
-            Scene mainScene = new Scene(scrollPane);
+            mainScene = new Scene(scrollPane);
             primaryStage.setScene(mainScene);
             primaryStage.setTitle("Sample JavaFX application");
             primaryStage.show();
@@ -26,5 +29,8 @@ public class Main extends Application {
             e.printStackTrace();
         }
     }
+        public static Scene getMainScene(){
+            return mainScene;
+        }
 
 }
